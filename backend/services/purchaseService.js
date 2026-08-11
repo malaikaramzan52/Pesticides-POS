abde77b3a86eaef991948ac95066c4ed5c84d45e
Pesticides Dev <user@pesticides.local>
2026-08-11 18:28:16 +0500

    Fix ReferenceError: mongoose is not defined in purchaseService.js

--- backend/services/purchaseService.js
+++ backend/services/purchaseService.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const PurchaseOrder = require('../models/PurchaseOrder');
 const PurchaseReturn = require('../models/PurchaseReturn');
 const Product = require('../models/Product');
